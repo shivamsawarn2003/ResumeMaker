d5552469b76bfb01b72dbef679f8c79d556155ad
Shivam <shivamsawarn2003@gmail.com>
2023-12-24 08:41:01 +0530

    Initialize project using Create React App

---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from "./Header.module.css";
+import resumeSvg from "../../assets/resume.svg";
+const Header = () => {
+  return (
+    <div className={styles.container}>
+        <p className={styles.heading}>
+            A <span>Resume</span>that stands out!
+        </p>
+        <p className={styles.heading}>
+            Make your own resume.<span>It's free</span>
+        </p>
+    
+    <div className={styles.right}>
+    <img src={resumeSvg} alt="Resume"/></div>
+    </div>
+  );
+}
+
+export default Header
